167c90c04a30a1d7c83cb2c7c7778545b74f2a9e
Daniel Badilla Jr <mail2danielb@gmail.com>
2025-09-24 16:27:55 -0700

    updated test.py to create function to print and added docstring; also added folders

--- test.py
+++ test.py
@@ -4,4 +4,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.signal
-print("All imports successful!")
+# print("All imports successful!")
+
+def main():
+    '''
+    This function, when called, prints "Hello, world!"
+
+    Arguments: None
+    '''
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
+    
